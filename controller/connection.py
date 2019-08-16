fcd60ac3096a02c1e25e8780537b6326da35b14c
guillaumezisa <guillaume.cyril.zisa@gmail.com>
2019-08-16 06:23:14 +0200

    Update fonctions de requetage et de connexion a la bdd

---
+++ controller/connection.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import psycopg2
+
+hostname = '172.19.0.2'
+username = 'postgres'
+password = 'facauchere'
+database = 'ipspawn'
+connect = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
+
+# FONCTION DE SELECT
+def select( connect , query ) :
+    db = connect.cursor()
+    db.execute( query )
+    print(db.fetchall())
+    connect.close()
+
+# FONCTION D'INSERT
+def insert( connect , query ) :
+    db = connect.cursor()
+    db.execute( query )
+    connect.commit()
+    connect.close()
+
+# A UTILISER DANS L'INSCRIPTION ECT
+query="INSERT INTO USERS (EMAIL,PSEUDO,PASSWORD,POINTS,PP,STATUS,DATES)VALUES ( 'zacsqcqsa', 'jacky', '123', 1, 'sex', 1, '2019-08-15');"
+insert( connect,query )
+
+query="SELECT PSEUDO,EMAIL FROM USERS"
+select( connect,query )
