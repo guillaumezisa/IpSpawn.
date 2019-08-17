24626fb880b28e0f5820a9dbd46cecb537e19fea
guillaumezisa <guillaume.cyril.zisa@gmail.com>
2019-08-18 00:24:52 +0200

    Update pycodestyle

--- controller/functions_database.py
+++ controller/functions_database.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
+
 # FUNCTION TO INSERT
-def insert( connect , query ) :
-      db = connect.cursor()
-      db.execute( query )
-      connect.commit()
+def insert(connect, query):
+    db = connect.cursor()
+    db.execute(query)
+    connect.commit()
--- controller/functions_subscribe.py
+++ controller/functions_subscribe.py
@@ -1,25 +1,28 @@
 #!/bin/usr/env python3
 
-# FUNCTION TO VERIFY IF THE PASSWORD IS CORRECT
-def verify_pass ( pass1 , pass2 ):
-    if ( pass1 != pass2 ):
+
+# FUNCTION TO VERIFY IF THE PASSWORD IS CORRECT -------------------------------
+def verify_pass(pass1, pass2):
+    if (pass1 != pass2):
         return 1
-    elif ( len(pass1) < 4 ) :
+    elif (len(pass1) < 4):
         return 2
-    elif ( len(pass1) > 50 ) :
+    elif (len(pass1) > 50):
         return 3
-    else :
+    else:
         return 0
 
-# FUNCTION TO VERIFY IF THE EMAIL IS NOT ALREADY EXISTING
-def verify_email ( connect,email,query ):
+
+# FUNCTION TO VERIFY IF THE EMAIL IS NOT ALREADY EXISTING ---------------------
+def verify_email(connect, email, query):
     db = connect.cursor()
     db_query = db.execute(query)
     db_email = db.fetchone()
     return db_email
 
-# FUNCTION TO VERIFY IF THE PSEUDO IS NOT ALREADY EXISTING
-def verify_pseudo ( connect,pseudo,query ):
+
+# FUNCTION TO VERIFY IF THE PSEUDO IS NOT ALREADY EXISTING --------------------
+def verify_pseudo(connect, pseudo, query):
     db = connect.cursor()
     db_query = db.execute(query)
     db_pseudo = db.fetchone()
@@ -27,4 +30,4 @@ def verify_pseudo ( connect,pseudo,query ):
 
 
 # FUNCTION XOR AND OTHERS FOR PASSWORDS
-#def secure_my_string ( string ):
+# def secure_my_string ( string ):
